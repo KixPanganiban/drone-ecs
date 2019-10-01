d7f312d798ba317ff53aa52ed7c676a7865b7743
Kix Panganiban <kixpanganiban@protonmail.com>
2019-10-01 21:31:02 +0800

    Fix nil pointer dereference

--- plugin.go
+++ plugin.go
@@ -383,12 +383,12 @@ func (p *Plugin) Exec() error {
 				break
 			}
 		}
-		runningCount := *newTaskSet.RunningCount
-		desiredCount := *newTaskSet.ComputedDesiredCount
+		runningCount := newTaskSet.RunningCount
+		desiredCount := newTaskSet.ComputedDesiredCount
 		fmt.Printf("Task Definition Arn: %s\n", newTaskDefinitionArn)
 		fmt.Printf("Running Count: %d\n", runningCount)
 		fmt.Printf("Desired Count: %d\n", desiredCount)
-		if runningCount == desiredCount {
+		if *runningCount == *desiredCount {
 			fmt.Println("Deployment done.")
 			break
 		} else {
